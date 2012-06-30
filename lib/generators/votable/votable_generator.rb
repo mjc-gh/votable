28319044a9eae4b7b58d1927a995359b180acfe4
mikeycgto <mikeycgto@gmail.com>
2012-06-30 16:03:53 -0400

    not needed

--- lib/generators/votable/votable_generator.rb
+++ lib/generators/votable/votable_generator.rb
@@ -3,7 +3,6 @@ module Votable
     class VotableGenerator < Rails::Generators::NamedBase
       include Rails::Generators::ResourceHelpers
 
-      source_root File.expand_path('../templates', __FILE__)
       desc "Generate a Vote model with the given NAME."
 
       namespace 'votable'
