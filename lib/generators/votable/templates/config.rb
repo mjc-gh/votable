9059bd4348d3fe98ed392b01ceb703f160873c58
mikeycgto <mikeycgto@gmail.com>
2012-06-30 16:07:22 -0400

    update config template

--- lib/generators/votable/templates/config.rb
+++ lib/generators/votable/templates/config.rb
@@ -1,8 +1,9 @@
 Votable.setup do |config|
   # The options and settings defined below are for global defaults.
-  # Each one of these options can be overriden when you call Votables 
-  # the votes_on and votable_by methods. Refer to the README for more
-  # details.
+  # All of these options can be locally overriden when you call Votable's 
+  # the "votes_on" method from a Voter class. 
+  #
+  # Refer to the README (and tests!) for more details.
 
   # set default Vote class
   # config.vote_class = 'Vote'
