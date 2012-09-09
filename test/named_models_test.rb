f0954b41570c4c10b450af2d0721004f272122d5
mikeycgto <mikeycgto@gmail.com>
2012-09-09 14:04:06 -0400

    add voted_on? named associations test

--- test/named_models_test.rb
+++ test/named_models_test.rb
@@ -17,6 +17,20 @@ class VotableNamedModelTest < ActiveSupport::TestCase
   end
 
   test "voted_on? handles two scoped associations" do
-    
+    u = create_user
+    c1 = create_comment
+    c2 = create_comment
+
+    u.cast_helpfull_comment_vote(c1, 5)
+    u.cast_quality_comment_vote(c1, -5)
+
+    u.cast_helpfull_comment_vote(c2, -5)
+    u.cast_quality_comment_vote(c2, 5)
+
+    assert u.helpfull_comment_votes.voted_on?(c1, :positive)
+    assert u.quality_comment_votes.voted_on?(c1, :negative)
+
+    assert u.helpfull_comment_votes.voted_on?(c2, :negative)
+    assert u.quality_comment_votes.voted_on?(c2, :positive)
   end
 end
